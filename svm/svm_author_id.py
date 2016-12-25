52378a09c19320ae8994d3186a017f65063eb627
Dewan <iit2015097@iiita.ac.in>
2016-12-26 00:35:47 +0530

    solution for SVM added

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,6 +10,8 @@
     
 import sys
 from time import time
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -25,6 +27,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-#########################################################
+t0 = time()
+clf = SVC(kernel="rbf", C = 1000000)
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print"training time:",round(time() - t0, 3),"s"
 
+t0 = time()
+print "10:",(clf.predict(features_test[10]))
+print "26:",(clf.predict(features_test[26]))
+print "50:",(clf.predict(features_test[50]))
+print"prediction time:",round(time() - t0, 3),"s"
+
+# print(accuracy_score(labels_code, labels_test))
+
+#########################################################
 
