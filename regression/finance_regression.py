0a3d0e36f9df23b71e17f61e34ce843e06e4dae6
Dewan <iit2015097@iiita.ac.in>
2017-01-21 11:33:33 +0530

    regression code changed

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -18,18 +18,20 @@ import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+from sklearn import linear_model
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
+sort_keys = '../tools/python2_lesson06_keys.pkl'
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,7 +39,11 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print(reg.score(feature_test, target_test ))
+print(reg.coef_)
+print(reg.intercept_)
 
 
 
