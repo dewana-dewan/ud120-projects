dceff89a64761ed34133643094e73162cff8b4e7
Dewan <iit2015097@iiita.ac.in>
2017-01-21 12:26:30 +0530

    working with outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,16 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
+    from operator import itemgetter
     cleaned_data = []
-
+    error = []
+    for i in range(0, len(ages)):
+        cleaned_data.append((ages[i], net_worths[i], abs(net_worths[i] - predictions[i])))
     ### your code goes here
-
-    
+    cleaned_data.sort(key = itemgetter(2))
+    print(cleaned_data)
+    cleaned_data = cleaned_data[:int(.9*len(cleaned_data))]  
+     
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -20,15 +20,19 @@ net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 ### and n_columns is the number of features
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
+
 from sklearn.cross_validation import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
 
-
-
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print(reg.coef_)
+print(reg.score(ages_test, net_worths_test))
 
 
 
@@ -46,6 +50,8 @@ plt.show()
 
 ### identify and remove the most outlier-y points
 cleaned_data = []
+predictions = reg.predict(ages_train)
+cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
 try:
     predictions = reg.predict(ages_train)
     cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
@@ -68,7 +74,7 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
-        plt.plot(ages, reg.predict(ages), color="blue")
+        plt.plot(ages, reg.predict(ages), color="red")
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
